93956397f6bc4af99c5fbf7b1e5b771500d71bf3
chudacontreras <chudacontreras@gmail.com>
2021-07-06 09:54:53 -0400

    aregado el install docker y modificado el Jenkinsfile y jenkins/install"

--- install-jenkins/install.sh
+++ install-jenkins/install.sh
@@ -1,9 +1,17 @@
 # Depending on your system you might need to use sudo
-apt-get update
-apt-get install openjdk-8-jdk wget gnupg
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
-apt-get update
-apt-get install git jenkins
-ssh-keygen
-service jenkins start
+sudo apt update
+sudo apt upgrade -y
+#agregar la clave del repositorio al sistema:
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+apt update
+apt install openjdk-11-jdk wget gnupg -y
+#sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
+#apt-get update
+sudo apt install git jenkins -y 
+
+#ssh-keygen
+#service jenkins start
